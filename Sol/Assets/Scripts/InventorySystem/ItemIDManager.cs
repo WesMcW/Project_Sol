4541cad2e59cd421f6740e12f19f6579b838b7e8
MrAndrewRobertMason <mrandrewrobertmason@gmail.com>
2019-09-14 21:54:20 -0500

    Added Item look up and item info component

---
+++ Sol/Assets/Scripts/InventorySystem/ItemIDManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemIDManager : MonoBehaviour
+{
+
+    #region Singleton 
+
+    public static ItemIDManager instance;
+
+    void Awake(){
+        if (instance != null){
+            Debug.LogWarning("More than one instance of ItemIDManager found");
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+
+    }
+
+    #endregion
+
+    [Header("Weapons")]
+    [SerializeField]
+    GameObject[] weapons;
+
+    [Header("Armor")]
+    [SerializeField]
+    GameObject[] armor;
+
+    [Header("Ingredients")]
+    [SerializeField]
+    GameObject[] ingredients;
+
+    [Header("Food")]
+    [SerializeField]
+    GameObject[] food;
+
+    //Jagged Hash Array
+    GameObject[][] array;
+
+    void Start() {
+        array = new GameObject[][] {
+            weapons,
+            armor,
+            ingredients,
+            food
+        };
+    }
+
+    //Call this to get item: ItemIDManager.instance.GetItem( idnumber );
+    public GameObject GetItem(int id) {
+        int type = GetTypeInt(id);
+        //if there are errors, switch the order of array parameters
+        return array[GetID(type, id)][type];
+    }
+
+    int GetTypeInt(int id) {
+        string s = id.ToString();
+        int i = int.Parse(s.Substring(0, 1));
+        return i;
+    }
+
+    int GetID(int firstValue, int i) {
+        int id = i - firstValue * (int)Mathf.Pow(10, CountDigits(i) - 1);
+        return id;
+    }
+
+    int CountDigits(int i) {
+        int count = 0;
+        while (i != 0) {
+            i = i / 10;
+            count++;
+        }
+        return count;
+    }
+}
---
+++ Sol/Assets/Scripts/InventorySystem/ItemInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInfo : MonoBehaviour
+{
+    public string name;
+    [TextArea(3, 5)]
+    public string description;
+    public Sprite sprite;
+
+}
