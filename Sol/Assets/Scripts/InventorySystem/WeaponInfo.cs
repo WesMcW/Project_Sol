707e6d584ed7bbedee076fb090dd6857c065ddd1
MrAndrewRobertMason <mrandrewrobertmason@gmail.com>
2019-11-03 17:46:36 -0600

    Moar swords

--- Sol/Assets/Scripts/InventorySystem/WeaponInfo.cs
+++ Sol/Assets/Scripts/InventorySystem/WeaponInfo.cs
@@ -6,8 +6,11 @@ public class WeaponInfo : ItemInfo
 {
 
     public int damage;
+    [Header("Additional % attack speed (100% is twice as fast)")]
+    [Header(".5 = 50%")]
     public float attackSpeed = 0; //Additional attack speed
 
+    [Header(".5 = 50%")]
     public float critChance = 0;
     public float critMultiplier = 0;
 
