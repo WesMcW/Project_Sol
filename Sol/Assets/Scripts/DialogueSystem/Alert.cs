d91167ed43be6e02a8bab938cf1d8549eb9c3bd9
Dylan <45637883+oelkeds21@users.noreply.github.com>
2019-11-05 19:44:22 -0600

    Touched Up Diallogue Alert and Added Alert art

--- Sol/Assets/Scripts/DialogueSystem/Alert.cs
+++ Sol/Assets/Scripts/DialogueSystem/Alert.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class Alert : MonoBehaviour
 {
-    
+    SpriteRenderer sr;
     private void OnEnable()
     {
         NPC_Dialogue npc = DialogueManager.DM.GetCurrentNPC();
-        SpriteRenderer sr = npc.transform.GetComponent<SpriteRenderer>() ?? npc.transform.parent.GetComponent<SpriteRenderer>();
-        this.transform.position = npc.transform.position + new Vector3(0, sr.bounds.size.y);
+        if (npc.transform.GetComponent<SpriteRenderer>())
+        {
+            sr = npc.transform.GetComponent<SpriteRenderer>();
+        }
+            
+        else
+        {
+            sr = npc.transform.GetComponentInParent<SpriteRenderer>();
+        }
+           
+        this.transform.position = npc.transform.position + new Vector3(0, sr.bounds.size.y * 1.2f);
     }
 }
