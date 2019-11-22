f3443e93194b6d424efe45968525cd47ffacb94f
WesMcW <wesmcwhorter@gmail.com>
2019-11-21 18:56:43 -0600

    j nal?

---
+++ Sol/Assets/JournalControls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JournalControls : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject questingInfo;
+
+    int pageNum;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void hideQuesting()
+    {
+        questingInfo.SetActive(false);
+    }
+
+    public void showQuesting()
+    {
+        questingInfo.SetActive(true);
+    }
+}
