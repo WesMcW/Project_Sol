88bd06b74912b9563922985f3b51361294b21b74
Dylan <45637883+oelkeds21@users.noreply.github.com>
2020-01-08 14:17:12 -0600

    Inventory Update and Gravity Item Fix

--- Sol/Assets/Scripts/ItemGravityZone.cs
+++ Sol/Assets/Scripts/ItemGravityZone.cs
@@ -7,18 +7,40 @@ public class ItemGravityZone : MonoBehaviour
     [SerializeField] float speed;
     private ItemInfo parentInfo;
     [SerializeField] private Transform parent;
+    bool gravitate;
+    Transform player;
 
     private void Awake()
     {
         parentInfo = GetComponentInParent<ItemInfo>();
     }
-    private void OnTriggerStay2D(Collider2D collision)
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (parentInfo.canBePickedUp && collision.CompareTag("Player"))
         {
             //Debug.Log("Player detected");
+            player = collision.transform;
+            gravitate = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (parentInfo.canBePickedUp && collision.CompareTag("Player"))
+        {
+            //Debug.Log("Player detected");
+            gravitate = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (gravitate)
+        {
+            //go towards player
             //Gravitate towards player
-            parent.position = Vector3.Lerp(parent.position, collision.gameObject.transform.position, speed * Time.deltaTime);
+            parent.position = Vector3.Lerp(parent.position, player.position, speed * Time.deltaTime);
         }
     }
 }
