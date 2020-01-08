e6490e084d6a7a32ac287bcb5bbdabd66b6c585b
WesMcW <wesmcwhorter@gmail.com>
2020-01-07 19:23:55 -0600

    Starting to work on the some quests

---
+++ Sol/Assets/Scripts/DummyHealth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyHealth : MonoBehaviour, IEnemy
+{
+    public int ID { get; set; }
+    public int Experience { get; set; }
+
+    public int maxHealth;
+
+    [SerializeField]
+    private int currentHealth;
+
+    void Start()
+    {
+        ID = 1;
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        CombatEvent.EnemyDied(this);
+        Destroy(gameObject);
+    }
+}
