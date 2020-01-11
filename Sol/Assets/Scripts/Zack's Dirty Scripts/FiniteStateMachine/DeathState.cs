55678ddd857a9791b84b5608b5b32c0c48930431
ZAG852 <36207290+ZAG852@users.noreply.github.com>
2020-01-11 17:39:13 -0600

    Added more states and stats
    working on them still but I have a basic outline of everything

---
+++ Sol/Assets/Scripts/Zack's Dirty Scripts/FiniteStateMachine/DeathState.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathState : FSMAction
+{
+    private string textToShow;
+    private float duration;
+    private float cachedDuration;
+    private string finishEvent;
+    EnemyAstar theEnemy;
+
+    public DeathState(FSMState owner) : base(owner)
+    {
+    }
+
+    public void Init(Transform target, string textToShow, float duration, EnemyAstar tE, string finishEvent = null)
+    {
+        this.textToShow = textToShow;
+        this.duration = duration;
+        this.cachedDuration = duration;
+        this.finishEvent = finishEvent;
+        this.theEnemy = tE;
+        
+    }
+    public override void OnEnter()
+    {
+        if (duration <= 0)
+        {
+            Finish();
+            return;
+        }
+    }
+
+    public override void OnUpdate()
+    {
+        duration -= Time.deltaTime;
+
+        if (duration <= 0)
+        {
+            Finish();
+            return;
+        }
+
+        Debug.Log(textToShow);
+    }
+
+    public override void OnExit()
+    {
+
+    }
+
+    public void Finish()
+    {
+        if (!string.IsNullOrEmpty(finishEvent))
+        {
+            GetOwner().SendEvent(finishEvent);
+        }
+        duration = cachedDuration;
+    }
+}
