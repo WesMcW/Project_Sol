96f077ec659a441bb68a6229e81ba6e79a2c10d4
Lebrra <Lebrra@users.noreply.github.com>
2020-01-11 18:42:59 -0600

    charge attack doesnt do damage yet

--- Sol/Assets/Scripts/Leah's Attempt/PlayerThings/BlastAttack.cs
+++ Sol/Assets/Scripts/Leah's Attempt/PlayerThings/BlastAttack.cs
@@ -7,6 +7,7 @@ public class BlastAttack : MonoBehaviour
     // damage pref for the blast attack
     // not sure how this will scale in respect to player yet, but should hit all enemies in radius; potentially multiple times
 
+    public Collider2D solidBox;
     float damage, critChance, critMulti;
 
     // Update is called once per frame
@@ -17,7 +18,7 @@ public class BlastAttack : MonoBehaviour
 
     public void setValues(GameObject player, float[] values)
     {
-        Physics2D.IgnoreCollision(player.GetComponent<BoxCollider2D>(), GetComponent<Collider2D>());
+        //Physics2D.IgnoreCollision(player.GetComponent<BoxCollider2D>(), solidBox);
         damage = values[0];
         critChance = values[1];
         critMulti = values[2];
@@ -30,10 +31,12 @@ public class BlastAttack : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log("hit " + collision.gameObject.name);
         // do damage, set enemy take damage cooldown
-        IEnemy enemy = collision.GetComponent<IEnemy>();
+        IEnemy enemy = collision.gameObject.GetComponent<IEnemy>();
         if (enemy != null)
         {
+            Debug.Log("enemy hit");
             if (Random.Range(0f, 1f) < critChance)
             {
                 enemy.TakeDamage((int)(damage * critMulti));
