cdda9387383f008be64e7500e4d56bc50d1ad97c
MrAndrewRobertMason <mrandrewrobertmason@gmail.com>
2020-01-12 22:06:16 -0600

    Chest Ahead

--- Sol/Assets/Scripts/InteractionSystem/LootContainer.cs
+++ Sol/Assets/Scripts/InteractionSystem/LootContainer.cs
@@ -8,13 +8,29 @@ public class LootContainer : Interactable
     [SerializeField] int[] possibleLoot;
     [Header("How much loot is in the container?")]
     [SerializeField] int lootAmount;
+    [SerializeField] bool saveState = false;
+    [SerializeField] string saveID = "0";
 
-    public override void Interact()
-    {
-        for (int i = 0; i < lootAmount; i++)
-        {
+
+    void Start() {
+        //0 is no interaction, 1 means we've meet before... havent we???
+        if (saveState) {
+            int state = PlayerPrefs.GetInt(saveID, 0);
+            if (state == 1){
+                Destroy(this.gameObject);
+            }
+        }
+        
+    }
+
+    public override void Interact(){
+        for (int i = 0; i < lootAmount; i++){
             GameObject tmp = Instantiate(ItemIDManager.instance.GetItem(possibleLoot[Random.Range(0, possibleLoot.Length - 1)]), this.transform.position, Quaternion.identity);
             
+        }
+
+        if (saveState) {
+            PlayerPrefs.SetInt(saveID, 1);
         }
 
         InteractionManager.IM.StopInteraction();
